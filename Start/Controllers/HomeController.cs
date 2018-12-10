3b5e1eabb25a13a4c0a79553079099823e1151a8
Navn <admin@TA-826-02>
2018-12-10 14:06:41 +0100

    Intro

---
+++ Start/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Start.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            string navn = "ladsækljdsjk";
+            return Json(navn, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
