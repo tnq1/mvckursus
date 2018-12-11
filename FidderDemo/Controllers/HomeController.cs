efe20665d3cd8b64804011438aa8d960dfa651df
Navn <admin@TA-826-02>
2018-12-11 15:44:35 +0100

    Dag 2

---
+++ FidderDemo/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using FidderDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FidderDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [OutputCache(Duration = 10)]
+        public ActionResult Index()
+        {
+           
+            ViewBag.nu = DateTime.Now.ToLongTimeString();
+            return View();
+        }
+
+        // GET: Home
+        [HttpGet]
+        public ActionResult RetPerson()
+        {
+
+            string sti = Server.MapPath("~/app_data/person.json");
+            Person p;
+            if (System.IO.File.Exists(sti))
+            {
+                string indhold = System.IO.File.ReadAllText(sti);
+                p = Newtonsoft.Json.JsonConvert.DeserializeObject<Person>(indhold);
+            }
+            else
+            {
+                p = new Person() { Id = 1 };
+            }
+            return View(p);
+        }
+
+        [HttpPost]
+        public ActionResult RetPerson(Person p)
+        {
+            // Gem
+           
+             if(ModelState.IsValid)
+            { 
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(p);
+
+            string sti = Server.MapPath("~/app_data/person.json");
+
+            System.IO.File.WriteAllText(sti, json);
+            }
+
+
+            return View();
+        }
+    }
+}
---
+++ FidderDemo/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FidderDemo.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Navn { get; set; }
+        public int Alder { get; set; }
+        public bool ErILive { get; set; }
+
+    }
+}
